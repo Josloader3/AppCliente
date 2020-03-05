128868cff6493bd6e7aa78e3c1047d1205fda810
Franklinjua07 <franjua.123@gmail.com>
2020-03-05 01:18:20 -0500

    Adición del tab que contendra a los Fragment (Cursos y Eventos). Adición del activity de maps.

--- app/src/main/java/com/cibertec/appcliente/activities/MainActivity.java
+++ app/src/main/java/com/cibertec/appcliente/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cibertec.appcliente.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -9,8 +11,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.cibertec.appcliente.Fragments.CursosFragment;
+import com.cibertec.appcliente.Fragments.EventosFragment;
 import com.cibertec.appcliente.R;
+import com.google.android.material.tabs.TabLayout;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +34,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TOOLBAR
+        Toolbar mainToolBar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(mainToolBar);
+
+        //AÑADIMOS EL PAGER ADAPTER
+        SectionsPagerAdapter pageAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        ViewPager pagerContenedor = findViewById(R.id.pagerContenedor);
+        pagerContenedor.setAdapter(pageAdapter);
+
+        //ADJUNTAMOS EL VIEWPAGER TO THE TABLAYOUT
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pagerContenedor);
     }
 
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager supportFragmentManager, int behaviorResumeOnlyCurrentFragment) {
+            super(supportFragmentManager,behaviorResumeOnlyCurrentFragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch ( position ){
+                case 0:
+                    return new CursosFragment();
+                case 1:
+                    return new EventosFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return getResources().getText(R.string.cursos_tab);
+                case 1:
+                    return getResources().getText(R.string.eventos_tab);
+            }
+            return null;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return false;
+        }
+    }
 }
